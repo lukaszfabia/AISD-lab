77ac881d62d4cb4036e5f4913574e4aa564d5060
Lukasz <ufabia03@gmail.com>
2023-03-07 12:03:29 +0100

    naprawa scannera

--- src/Document.java
+++ src/Document.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 
 public class Document {
     public static void loadDocument(String name, Scanner scan) {
-        Scanner scanner = new Scanner(System.in);
         String line = "";
         List<String> list = new ArrayList<>();
 
         while (!line.equals("eod")) {
-            line = scanner.nextLine();
+            line = scan.nextLine();
 
             String[] words = line.split("\\s+");
             for (String word : words) {
                 if (correctLink(word.toLowerCase())) {
                     list.add(word.substring(5).toLowerCase());
-                    //System.out.println(word.substring(5).toLowerCase());
                 }
             }
         }
@@ -24,9 +22,7 @@ public class Document {
             System.out.println(s);
         }
     }
-
-
-    public static boolean correctLink(String link) {
+    private static boolean correctLink(String link) {
         return link.matches("link=[a-zA-Z][a-zA-Z0-9_]*");
     }
 }
