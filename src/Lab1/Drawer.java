d48e5cd79b62e7c58cfac4a1cb4df2b1b6059116
Lukasz <ufabia03@gmail.com>
2023-03-10 13:53:02 +0100

    new items :O

--- src/Lab1/Drawer.java
+++ src/Lab1/Drawer.java
@@ -1,7 +1,14 @@
 package Lab1;
 
 public class Drawer {
-
+    public static void main(String[] args) {
+        drawSq(2);
+        drawSq(5);
+        System.out.println();
+        drawChristmassTree(4);
+        System.out.println();
+        drawPyramid(4);
+    }
     private static void drawLine(int n, char ch) {
         for (int i = 1; i <= n; i++) {
             System.out.print(ch);
@@ -9,23 +16,34 @@ public class Drawer {
     }
 
     public static void drawPyramid(int n) {
-        for (int i = 1; i <= n; i++) {
-            drawLine(n - i, '.');
-            drawLine(2 * i - 1, 'X');
-            drawLine(n - i, '.');
+        drawPyramidO(n,n);
+    }
+
+    private static void drawPyramidO(int i, int n){
+        for (int j = 1; j <= i; j++) {
+            drawLine(n - j, '.');
+            drawLine(2 * j - 1, 'X');
+            drawLine(n - j, '.');
             System.out.println();
         }
     }
 
     public static void drawChristmassTree(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <=i; j++) {
-                drawLine(n - j, '.');
-                drawLine(2 * j - 1, 'X');
-                drawLine(n - j, '.');
-                System.out.println();
-            }
+            drawPyramidO(i, n);
         }
+    }
+
+    public static void drawSq(int n) {
+        drawLine(n, 'x');
+        System.out.println();
+        for (int i = 0; i < n - 2; i++) {
+            System.out.print('x');
+            drawLine(n - 2, '.');
+            System.out.println('x');
+        }
+        drawLine(n, 'x');
+        System.out.println();
     }
 
 
